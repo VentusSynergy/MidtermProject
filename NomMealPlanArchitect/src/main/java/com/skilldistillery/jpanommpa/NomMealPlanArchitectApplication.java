6479568384a14df4b850dc39e32eafef08b10597
Vinton Lee <vinton.e.lee@gmail.com>
2019-11-14 14:16:44 -0700

    initial stub of boot

---
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/NomMealPlanArchitectApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpanommpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomMealPlanArchitectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomMealPlanArchitectApplication.class, args);
+	}
+
+}
---
+++ NomMealPlanArchitect/src/test/java/com/skilldistillery/jpanommpa/NomMealPlanArchitectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpanommpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomMealPlanArchitectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
