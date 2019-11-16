f1deaa64d9ae37935057e5e8623082c669d8451b
tduplantis83 <tduplantis83@gmail.com>
2019-11-16 13:44:27 -0700

    Completed UserRecipeFavDAO & Impl - pending testing

---
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/dao/UserRecipeFavoritesDAO.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jpanommpa.dao;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import com.skilldistillery.jpanommpa.entities.UserRecipe;
+
+public interface UserRecipeFavoritesDAO {
+	public UserRecipe createUserRecipe(UserRecipe ur);
+	public UserRecipe updateUserRecipe(UserRecipe ur);
+	public boolean deleteUserRecipe(UserRecipe ur);
+	public List<UserRecipe> selectAllUserRecipe();
+	public List<UserRecipe> selectUserRecipeByRecipeName(String name);
+	public List<UserRecipe> selectUserRecipeByDateLastMade(LocalDate date);
+
+}
---
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/dao/UserRecipeFavoritesDAOImpl.java
@@ -0,0 +1,86 @@
+package com.skilldistillery.jpanommpa.dao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.jpanommpa.entities.UserRecipe;
+@Transactional
+@Service
+public class UserRecipeFavoritesDAOImpl implements UserRecipeFavoritesDAO{
+	@PersistenceContext
+	EntityManager em;
+
+	@Override
+	public UserRecipe createUserRecipe(UserRecipe ur) {
+		em.persist(ur);
+		
+		em.flush();
+		
+		return ur;
+	}
+
+	@Override
+	public UserRecipe updateUserRecipe(UserRecipe ur) {
+		UserRecipe matchingUR = em.find(UserRecipe.class, ur.getId());
+
+		matchingUR.setUser(ur.getUser());
+		matchingUR.setRecipe(ur.getRecipe());
+		matchingUR.setUserFavorite(true);
+		matchingUR.setComment(ur.getComment());
+		matchingUR.setDateLastMade(ur.getDateLastMade());
+		
+
+		em.persist(matchingUR);
+
+		em.flush();
+
+		return matchingUR;
+	}
+
+	@Override
+	public boolean deleteUserRecipe(UserRecipe ur) {
+		UserRecipe matchingUR = em.find(UserRecipe.class, ur.getId());
+		
+		matchingUR.setUserFavorite(false);
+		
+		em.persist(ur);
+		
+		em.flush();
+
+		return true;
+	}
+
+	@Override
+	public List<UserRecipe> selectAllUserRecipe() {
+		String query = "Select u from UserRecipe";
+
+		List<UserRecipe> results = em.createQuery(query, UserRecipe.class).getResultList();
+
+		return results;
+	}
+
+	@Override
+	public List<UserRecipe> selectUserRecipeByRecipeName(String name) {
+		String query = "Select u from UserRecipe where u.recipe.name = :name";
+
+		List<UserRecipe> results = em.createQuery(query, UserRecipe.class).setParameter("name", name).getResultList();
+
+		return results;
+	}
+
+	@Override
+	public List<UserRecipe> selectUserRecipeByDateLastMade(LocalDate date) {
+		String query = "Select u from UserRecipe where u.dateLastMade = :date";
+
+		List<UserRecipe> results = em.createQuery(query, UserRecipe.class).setParameter("date", date).getResultList();
+
+		return results;
+	}
+
+}
