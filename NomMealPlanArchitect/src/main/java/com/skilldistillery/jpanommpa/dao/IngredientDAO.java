831c2e45af0b1bed82b88d19186c6d9bb3db8d61
tduplantis83 <tduplantis83@gmail.com>
2019-11-15 17:52:07 -0700

    IngredientDAO & IngredientDAOImpl done - waiting testing.

---
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/dao/IngredientDAO.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.jpanommpa.dao;
+
+import java.util.*;
+
+import com.skilldistillery.jpanommpa.entities.Ingredient;
+
+public interface IngredientDAO {
+	public Ingredient createIngredient(Ingredient i);
+	public List<Ingredient> selectAllIngredient();
+	public List<Ingredient> selectIngredientByName(String name);
+	public List<Ingredient> selectIngredientByBrand(String brand);
+	public List<Ingredient> selectIngredientByCategory(String category);
+	public Ingredient updateIngredient(Ingredient i);
+	public boolean deleteIngredient(Ingredient i);
+}
