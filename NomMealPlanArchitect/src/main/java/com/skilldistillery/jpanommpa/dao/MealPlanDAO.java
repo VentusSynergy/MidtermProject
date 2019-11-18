8ed9c4e416b16d0f08fc49ee182036b6e5a5ee1e
Kullen <kullen.kee@outlook.com>
2019-11-18 16:41:36 -0700

    connected meal plan

--- NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/dao/MealPlanDAO.java
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/dao/MealPlanDAO.java
@@ -3,11 +3,18 @@ package com.skilldistillery.jpanommpa.dao;
 import java.util.*;
 
 import com.skilldistillery.jpanommpa.entities.MealPlan;
+import com.skilldistillery.jpanommpa.entities.User;
 
 public interface MealPlanDAO {
 	public MealPlan createMealPlan(MealPlan m);
+
 	public MealPlan updateMealPlan(MealPlan m);
+
 	public boolean deleteMealPlan(MealPlan m);
+
 	public List<MealPlan> selectAllMealPlan(MealPlan m);
+
 	public List<MealPlan> selectMealPlanByKeyword(String keyword);
+
+	List<MealPlan> selectMealPlanByUser(User user);
 }
