c5b2400829325cdcb47f13fda76bd8da1cf4a8ad
Kullen <kullen.kee@outlook.com>
2019-11-21 15:47:51 -0700

    live server

--- NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/NomMealPlanArchitectApplication.java
+++ NomMealPlanArchitect/src/main/java/com/skilldistillery/jpanommpa/NomMealPlanArchitectApplication.java
@@ -4,16 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EntityScan("com.skilldistillery.jpanommpa")
-public class NomMealPlanArchitectApplication {
-
+public class NomMealPlanArchitectApplication extends SpringBootServletInitializer {
+	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(NomMealPlanArchitectApplication
-				.class);
+		return application.sources(NomMealPlanArchitectApplication.class);
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(NomMealPlanArchitectApplication.class, args);
 	}
